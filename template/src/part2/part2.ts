b1be349344d0de8351d3935260b5ea37ea412bc2
kareem2134 <kareemhajyahya2003@gmail.com>
2025-04-15 05:39:02 +0300

    added types to the functions in 2.2

--- template/src/part2/part2.ts
+++ template/src/part2/part2.ts
@@ -8,10 +8,10 @@ const helpFunction:(char:string)=>boolean = (char:string) => vowels.includes(cha
 export const countVowels:(str:string)=> number = (str : string) => stringToArray(str).filter(helpFunction).length
 
 /* Question 2 */
-const isLetter = (char : string) => ((char.charCodeAt(0) > 64) && char.charCodeAt(0) < 91) || (char.charCodeAt(0) > 96 && char.charCodeAt(0) < 123)
-const fromUpperToLower = (char : string) => (char.charCodeAt(0) > 64 && char.charCodeAt(0) < 91) ? String.fromCharCode((char.charCodeAt(0)+32)) : char
-const checkIfPilandrome:(array :string[],a : number, b:number)=>boolean = (array :string[],a : number, b:number) => (b-a <= 0) ? true : ((array[a] == array[b]) ? checkIfPilandrome(array,a+1,b+1): false) 
-export const isPalindrome = (str : string) => checkIfPilandrome(stringToArray(str).filter(isLetter).map(fromUpperToLower),0,stringToArray(str).filter(isLetter).map(fromUpperToLower).length)
+const isLetter:((char : string) => boolean) = (char : string) => ((char.charCodeAt(0) > 64) && char.charCodeAt(0) < 91) || (char.charCodeAt(0) > 96 && char.charCodeAt(0) < 123)
+const fromUpperToLower:(char : string) => string = (char : string) => (char.charCodeAt(0) > 64 && char.charCodeAt(0) < 91) ? String.fromCharCode((char.charCodeAt(0)+32)) : char
+const checkIfPilandrome:(array :string[],a : number, b:number) => boolean = (array :string[],a : number, b:number) => (b-a <= 0) ? true : ((array[a] == array[b]) ? checkIfPilandrome(array,a+1,b+1): false) 
+export const isPalindrome:(str : string) => boolean = (str : string) => checkIfPilandrome(stringToArray(str).filter(isLetter).map(fromUpperToLower),0,stringToArray(str).filter(isLetter).map(fromUpperToLower).length)
   
 /* Question 3 */
 export type WordTree =
