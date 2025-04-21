28d2caed67e8748327e76dae5b6b09e6b69816b1
kareem2134 <kareemhajyahya2003@gmail.com>
2025-04-21 20:15:59 +0300

    fix a bug in part 2 in function isPalindroome

--- template/src/part2/part2.ts
+++ template/src/part2/part2.ts
@@ -10,8 +10,8 @@ export const countVowels:(str:string)=> number = (str : string) => stringToArray
 /* Question 2 */
 const isLetter:((char : string) => boolean) = (char : string) => ((char.charCodeAt(0) > 64) && char.charCodeAt(0) < 91) || (char.charCodeAt(0) > 96 && char.charCodeAt(0) < 123)
 const fromUpperToLower:(char : string) => string = (char : string) => (char.charCodeAt(0) > 64 && char.charCodeAt(0) < 91) ? String.fromCharCode((char.charCodeAt(0)+32)) : char
-const checkIfPilandrome:(array :string[],a : number, b:number) => boolean = (array :string[],a : number, b:number) => (b-a <= 0) ? true : ((array[a] == array[b]) ? checkIfPilandrome(array,a+1,b+1): false) 
-export const isPalindrome:(str : string) => boolean = (str : string) => checkIfPilandrome(stringToArray(str).filter(isLetter).map(fromUpperToLower),0,stringToArray(str).filter(isLetter).map(fromUpperToLower).length)
+const checkIfPilandrome:(array :string[],a : number, b:number) => boolean = (array :string[],a : number, b:number) => (b-a <= 0) ? true : ((array[a] == array[b]) ? checkIfPilandrome(array,a+1,b-1): false) 
+export const isPalindrome:(str : string) => boolean = (str : string) => checkIfPilandrome(stringToArray(str).filter(isLetter).map(fromUpperToLower),0,stringToArray(str).filter(isLetter).map(fromUpperToLower).length-1)
   
 /* Question 3 */
 export type WordTree =
@@ -20,4 +20,4 @@ export type WordTree =
     children: WordTree[];
 }
 
-export const treeToSentence:((tree : WordTree) => string) = (tree : WordTree) => (tree.children.length == 0) ? tree.root : tree.root+" "+tree.children.reduce((acc, curr) => acc + treeToSentence(curr)+" ","")
+export const treeToSentence:((tree : WordTree) => string) = (tree : WordTree) => (tree.children.length == 0) ? tree.root : tree.root+" "+tree.children.map(treeToSentence).join(" ")
